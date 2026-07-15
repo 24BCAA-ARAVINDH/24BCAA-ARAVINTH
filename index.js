91d8b7793d5aedff67136723d0d5447b3dd9c29d
24BCAA-ARAVINDH <aravindhs1406@gmail.com>
2026-07-15 10:23:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,6 @@
 // Tag name: h1
 // Text: Welcome to React
 
-constelement=React.createElement("h1",nulll,"Welcome to react");
-constroot = ReactDOM.creatRoot(doucment.getElementById("root"));
+const element=React.createElement("h1","Welcome to react");
+const root = ReactDOM.creatRoot(doucment.getElementById("root"));
 root.render(element);
