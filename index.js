4bb34ef0beb0a711f99e1382e834c0f585143484
24BCAA-ARAVINDH <aravindhs1406@gmail.com>
2026-07-15 10:06:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,6 @@
 // Tag name: h1
 // Text: Welcome to React
 
-const element=React.createElement("h1,null,"Welcome to my web page");
+const element=React.createElement("h1" nulll,"Welcome to my web page");
 const root = ReactDom.creatRoot(doucment. getElementById);
 root.render(element);
